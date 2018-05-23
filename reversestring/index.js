bbecb9ba7a4e22446338222135429a3d5039a44a
AliGorithm <alisher.coder@gmail.com>
2018-05-22 22:15:48 -0500

    reverseStringUsingReduce

--- reversestring/index.js
+++ reversestring/index.js
@@ -21,7 +21,7 @@ function reverse02(str) {
     return str.split('').reverse().join('');
 }
 
-// reverse using the for loop
+// reverse using the for loop 
 function reverse03(str) {
     let reversed = '';
     for (let char of str) {
@@ -30,4 +30,11 @@ function reverse03(str) {
     return reversed;
 }
 
-module.exports = reverse03;
+// reverse using the reduce()  
+function reverse04(str) {
+    return str.split('').reduce((reversed, char) => {
+        return char + reversed;
+    }, '');
+}
+
+module.exports = reverse04;
